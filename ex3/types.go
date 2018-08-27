c0c62db2d84de0c12cd7b0d602eec039cbc11d05
Muhammad Falak R Wani <falakreyaz@gmail.com>
2018-08-28 02:20:14 +0530

    ex3: add path func functional option

--- ex3/types.go
+++ ex3/types.go
@@ -32,26 +32,40 @@ func WithTemplate(t *template.Template) HandlerOption {
 	}
 }
 
+func WithPathFn(fn func(r *http.Request) string) HandlerOption {
+	return func(h *handler) {
+		h.pathFn = fn
+	}
+}
+
 func NewHandler(s Story, opts ...HandlerOption) http.Handler {
-	h := handler{s, tmpl}
+	h := handler{
+		s:      s,
+		t:      tmpl,
+		pathFn: defaultPathFn,
+	}
 	for _, o := range opts {
 		o(&h)
 	}
 	return h
 }
 
-type handler struct {
-	s Story
-	t *template.Template
-}
-
-func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func defaultPathFn(r *http.Request) string {
 	path := strings.TrimSpace(r.URL.Path)
 	if path == "" || path == "/" {
 		path = "/intro"
 	}
 	path = path[1:] // Trime the leading /
+}
 
+type handler struct {
+	s      Story
+	t      *template.Template
+	pathFn func(r *http.Request) string
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := h.pathFn(r)
 	if chapter, ok := h.s[path]; ok {
 		err := h.t.Execute(w, chapter)
 		if err != nil {
