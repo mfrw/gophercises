61cf90e4b162520c8009f089aed0a78de9bb7323
Muhammad Falak R Wani <falakreyaz@gmail.com>
2018-08-29 06:21:25 +0530

    ex4: add support to extract text

--- ex4/link.go
+++ ex4/link.go
@@ -1,8 +1,8 @@
 package ex4
 
 import (
-	"fmt"
 	"io"
+	"strings"
 
 	"golang.org/x/net/html"
 )
@@ -20,10 +20,40 @@ func Parse(r io.Reader) ([]Link, error) {
 		return nil, err
 	}
 	nodes := linkNodes(doc)
+	var links []Link
 	for _, node := range nodes {
-		fmt.Println(node)
+		links = append(links, buildLink(node))
 	}
-	return nil, nil
+	return links, nil
+}
+
+func text(n *html.Node) string {
+	if n.Type == html.TextNode {
+		return n.Data
+	}
+	if n.Type != html.ElementNode {
+		return ""
+	}
+
+	var ret string
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret += text(c) + " " // Optimize this using a byte.Buffer or string.Builder
+	}
+
+	return strings.Join(strings.Fields(ret), " ")
+}
+
+func buildLink(n *html.Node) Link {
+	var ret Link
+	for _, attr := range n.Attr {
+		if attr.Key == "href" {
+			ret.Href = attr.Val
+			break
+		}
+	}
+	ret.Text = text(n)
+	return ret
 }
 
 func linkNodes(n *html.Node) []*html.Node {
