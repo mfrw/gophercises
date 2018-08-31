b9d0dee6e68e5ba6f9b1d393526565007b85d6a4
Muhammad Falak R Wani <falakreyaz@gmail.com>
2018-09-01 03:44:29 +0530

    ex9: add sorting and shuffling

--- ex9/card.go
+++ ex9/card.go
@@ -1,6 +1,10 @@
 package deck
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+)
 
 //go:generate stringer -type=Suit,Rank
 type Suit uint
@@ -14,6 +18,8 @@ const (
 )
 
 type Rank uint8
+
+var suits = [...]Suit{Spade, Diamond, Club, Heart}
 
 const (
 	_ Rank = iota
@@ -32,6 +38,11 @@ const (
 	King
 )
 
+const (
+	minRank = Ace
+	maxRank = King
+)
+
 type Card struct {
 	Suit
 	Rank
@@ -41,5 +52,56 @@ func (c Card) String() string {
 	if c.Suit == Joker {
 		return c.Suit.String()
 	}
-	return fmt.Sprintf("%s of %s", c.Rank.String(), c.Suit.String())
+	return fmt.Sprintf("%s of %ss", c.Rank.String(), c.Suit.String())
+}
+
+func New(opts ...func([]Card) []Card) []Card {
+	var cards []Card
+	for _, suit := range suits {
+		for rank := minRank; rank <= maxRank; rank++ {
+			cards = append(cards, Card{Suit: suit, Rank: rank})
+		}
+	}
+	for _, opt := range opts {
+		cards = opt(cards)
+	}
+	return cards
+}
+
+func DefaultSort(cards []Card) []Card {
+	sort.Slice(cards, Less(cards))
+	return cards
+}
+
+func Sort(less func(cards []Card) func(i, j int) bool) func([]Card) []Card {
+	return func(cards []Card) []Card {
+		sort.Slice(cards, less(cards))
+		return cards
+	}
+}
+
+func Less(cards []Card) func(i, j int) bool {
+	return func(i, j int) bool {
+		return absRank(cards[i]) < absRank(cards[j])
+	}
+}
+
+func absRank(c Card) int {
+	return int(c.Suit)*int(maxRank) + int(c.Rank)
+}
+
+func Shuffle(cards []Card) []Card {
+	rand.Shuffle(len(cards), func(i, j int) {
+		cards[i], cards[j] = cards[j], cards[i]
+	})
+	return cards
+}
+
+func Jokers(n int) func([]Card) []Card {
+	return func(cards []Card) []Card {
+		for i := 0; i < n; i++ {
+			cards = append(cards, Card{Rank: Rank(i), Suit: Joker})
+		}
+		return cards
+	}
 }
--- ex9/card_test.go
+++ ex9/card_test.go
@@ -1,9 +1,41 @@
 package deck
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
 
 func ExampleCard() {
 	fmt.Println(Card{Rank: Ace, Suit: Heart})
 	//Output:
-	// Ace of Heart
+	// Ace of Hearts
+}
+
+func TestNew(t *testing.T) {
+	cards := New()
+	if len(cards) != 13*4 {
+		t.Error("Wrong Number of cards in a new deck.")
+	}
+}
+
+func TestDefaultSort(t *testing.T) {
+	cards := New(DefaultSort)
+	exp := Card{Rank: Ace, Suit: Spade}
+	if exp != cards[0] {
+		t.Error("Sorting messed up")
+	}
+}
+
+func TestJokers(t *testing.T) {
+	cards := New(Jokers(3))
+	count := 0
+
+	for _, card := range cards {
+		if card.Suit == Joker {
+			count++
+		}
+	}
+	if count != 3 {
+		t.Error("Incorrect number of Jokers")
+	}
 }
