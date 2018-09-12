dca36b0cb9d94f1d5f29bc7dd923cdad61c9d247
Muhammad Falak R Wani <falakreyaz@gmail.com>
2018-09-12 20:11:02 +0530

    ex18: use the primitve package (refactor)

--- ex18/main.go
+++ ex18/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"time"
 
 	"github.com/mfrw/gophercises/ex18/primitive"
@@ -13,10 +14,29 @@ func trackTime(s time.Time, msg string) {
 	fmt.Println(msg, ":", e)
 }
 
+type mw func(w http.ResponseWriter, r *http.Request)
+
+func auth(w http.ResponseWriter, r *http.Request) {
+	c, err := r.Cookie("auth")
+}
+
+func auth(next mw) mw {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "http://google.com/", http.StatusFound)
+}
+
 func main() {
-	b, err := primitive.Primitive("test.png", "out.png", 10, ModeTriangle)
+	b, err := primitive.Primitive("test.png", "out.png", 10, primitive.ModeTriangle)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(b)
+
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8080", nil)
 }
