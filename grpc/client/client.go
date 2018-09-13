4bee5681e6e1d188b726217cf73a1eb2303a003d
Muhammad Falak R Wani <falakreyaz@gmail.com>
2018-09-13 13:10:22 +0530

    grpc: add client and proto defintion

---
+++ grpc/client/client.go
@@ -0,0 +1,157 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"io"
+	"log"
+	"math/rand"
+	"time"
+
+	pb "github.com/mfrw/gophercises/grpc/routeguide"
+	grpc "grpc.go4.org"
+	"grpc.go4.org/credentials"
+)
+
+var (
+	tls                = flag.Bool("tls", false, "Connection uses TLS")
+	caFile             = flag.String("ca_file", "", "CA root cert")
+	serverAddr         = flag.String("server_addr", "127.0.0.1:10000", "The server addr")
+	serverHostOverride = flag.String("server_host_override", "x.test.youtube.com", "The server name to use to verify the hostname")
+)
+
+func printFeature(client pb.RouteGuideClient, point *pb.Point) {
+	log.Printf("Getting feature for point (%d, %d)", point.Latitude, point.Longitute)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	feature, err := client.GetFeature(ctx, point)
+	if err != nil {
+		log.Fatal("%v.GetFeatures(_) = _, %v: ", client, err)
+	}
+	log.Println(feature)
+}
+
+func printFeatures(client pb.RouteGuideClient, rect *pb.Rectangle) {
+	log.Printf("Looking for features withing %v", rect)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	stream, err := client.ListFeatures(ctx, rect)
+	if err != nil {
+		log.Fatal("%v.ListtFeatures(_) = _, %v: ", client, err)
+	}
+	for {
+		feature, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal("%v.ListtFeatures(_) = _, %v", client, err)
+		}
+		log.Println(feature)
+	}
+}
+
+func runRecordRoute(client pb.RouteGuideClient) {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	pointCount := int(r.Int31n(100)) + 2
+	var points []*pb.Point
+	for i := 0; i < pointCount; i++ {
+		points = append(points, randomPoint(r))
+	}
+	log.Printf("Traversing %d points", len(points))
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	stream, err := client.RecordRoute(ctx)
+	if err != nil {
+		log.Fatal("%v.RecordRoute(_) = _, %v", client, err)
+	}
+	for _, point := range points {
+		if err := stream.Send(point); err != nil {
+			log.Fatal("%v.Send(%v) = %v", stream, point, err)
+		}
+	}
+	reply, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatal("%v.CloseAndRecv() got error %v, want %v", stream, err, nil)
+	}
+	log.Printf("Route summary: %v", reply)
+}
+
+func runRouteChat(client pb.RouteGuideClient) {
+	notes := []*pb.RouteNotes{
+		{Location: &pb.Point{Latitude: 0, Longitute: 1}, Message: "First message"},
+		{Location: &pb.Point{Latitude: 0, Longitute: 2}, Message: "Second message"},
+		{Location: &pb.Point{Latitude: 0, Longitute: 3}, Message: "Third message"},
+		{Location: &pb.Point{Latitude: 0, Longitute: 1}, Message: "Fourth message"},
+		{Location: &pb.Point{Latitude: 0, Longitute: 2}, Message: "Fifth message"},
+		{Location: &pb.Point{Latitude: 0, Longitute: 3}, Message: "Sixth message"},
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	stream, err := client.RouteChat(ctx)
+	if err != nil {
+		log.Fatal("%v.RouteChat(_) = _, %v", client, err)
+	}
+	waitc := make(chan struct{})
+	go func() {
+		for {
+			in, err := stream.Recv()
+			if err == io.EOF {
+				close(waitc)
+				return
+			}
+			if err != nil {
+				log.Fatal("Failed to recieve a note: %v", err)
+			}
+			log.Printf("Got a message %s at point (%d, %d)", in.Message, in.Location.Latitude, in.Location.Longitute)
+		}
+	}()
+	for _, note := range notes {
+		if err := stream.Send(note); err != nil {
+			log.Fatal("Failed to send a note: %v", err)
+		}
+	}
+	stream.CloseSend()
+	<-waitc
+}
+
+func randomPoint(r *rand.Rand) *pb.Point {
+	lat := (r.Int31n(180) - 90) * 1e7
+	long := (r.Int31n(360) - 180) * 1e7
+	return &pb.Point{Latitude: lat, Longitute: long}
+}
+
+func main() {
+	flag.Parse()
+	var opts []grpc.DialOption
+	if *tls {
+		if *caFile == "" {
+			log.Fatal("Provide a ca file")
+		}
+		creds, err := credentials.NewClientTLSFromFile(*caFile, *serverHostOverride)
+		if err != nil {
+			log.Fatalf("Failed to create TLS creds %v", err)
+		}
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+	} else {
+		opts = append(opts, grpc.WithInsecure())
+	}
+
+	conn, err := grpc.Dial(*serverAddr, opts...)
+	if err != nil {
+		log.Fatal("failed to dial: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewRouteGuideClient(conn)
+
+	printFeature(client, &pb.Point{Latitude: 409146138, Longitute: -746188906})
+
+	printFeatures(client, &pb.Rectangle{
+		Lo: &pb.Point{Latitude: 400000000, Longitute: -750000000},
+		Hi: &pb.Point{Latitude: 420000000, Longitude: -730000000},
+	})
+
+	runRecordRoute(client)
+	runRouteChat(client)
+}
