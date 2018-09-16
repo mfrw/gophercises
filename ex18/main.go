1ace82a636dab41697135e5dc231b764be51f0a0
Muhammad Falak R Wani <falakreyaz@gmail.com>
2018-09-16 16:03:46 +0530

    ex18: modify main to upload file

--- ex18/main.go
+++ ex18/main.go
@@ -2,41 +2,50 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
-	"time"
+	"path/filepath"
 
 	"github.com/mfrw/gophercises/ex18/primitive"
 )
 
-func trackTime(s time.Time, msg string) {
-	e := time.Since(s)
-	fmt.Println(msg, ":", e)
-}
-
-type mw func(w http.ResponseWriter, r *http.Request)
-
-func auth(w http.ResponseWriter, r *http.Request) {
-	c, err := r.Cookie("auth")
-}
-
-func auth(next mw) mw {
-	return func(w http.ResponseWriter, r *http.Request) {
-
-	}
-}
-
-func index(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "http://google.com/", http.StatusFound)
-}
-
 func main() {
-	b, err := primitive.Primitive("test.png", "out.png", 10, primitive.ModeTriangle)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(b)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		html := `<html><body>
+			<form action="/upload" method="post" enctype="multipart/form-data">
+				<input type="file" name="image">
+				<button type="submit">Upload Image</button>
+			</form>
+			</body></html>`
+		fmt.Fprint(w, html)
+	})
+	mux.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
+		file, header, err := r.FormFile("image")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		defer file.Close()
+		ext := filepath.Ext(header.Filename)[1:]
+		out, err := primitive.Transform(file, ext, 33)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		switch ext {
+		case "jpg":
+			fallthrough
+		case "jpeg":
+			w.Header().Set("Content-Type", "image/jpeg")
+		case "png":
+			w.Header().Set("Content-Type", "image/png")
+		default:
+			http.Error(w, fmt.Sprintf("Invalid image type %s", ext), http.StatusBadRequest)
 
-	http.HandleFunc("/", index)
-	http.ListenAndServe(":8080", nil)
+		}
+		io.Copy(w, out)
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
