9e3b23b0035b1f0821764a544811f38a0db3b77c
Muhammad Falak R Wani <falakreyaz@gmail.com>
2018-09-16 23:11:35 +0530

    asyncnet: initial blocking implementation

---
+++ nwasync/asyncnet.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	l, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		n, err := io.Copy(os.Stderr, conn)
+		log.Printf("Copied %d bytes; finished with err = %v\n", n, err)
+	}
+}
