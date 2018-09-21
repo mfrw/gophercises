0f084e380240426521b2415e1ee97d2fa4c5db9d
Muhammad Falak R Wani <falakreyaz@gmail.com>
2018-09-22 04:54:06 +0530

    map: test with strings

--- map/main.go
+++ map/main.go
@@ -1,8 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func main() {
+func map1() {
 	results := map[string]int{}
 	for i := 0; i < 100000; i++ {
 		m := make(map[int]string)
@@ -41,4 +44,25 @@ func main() {
 		results[str]++
 	}
 	fmt.Println(len(results))
+}
+
+func map2() {
+	m := map[string]int{
+		"G": 7, "A": 1, "C": 3, "E": 5,
+		"D": 4, "B": 2, "F": 6, "I": 9, "H": 8,
+	}
+	counts := map[string]int{}
+	for i := 0; i < 1000000; i++ {
+		var order strings.Builder
+		for k, _ := range m {
+			order.WriteString(k)
+		}
+		counts[order.String()]++
+	}
+	fmt.Println(len(counts))
+}
+
+func main() {
+	map1()
+	map2()
 }
