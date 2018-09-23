671f11befac219051815aa7f6482adeb933dcc60
Muhammad Falak R Wani <falakreyaz@gmail.com>
2018-09-23 08:48:40 +0530

    http: add simple instrumentation with prometheus.

--- http/main.go
+++ http/main.go
@@ -2,14 +2,74 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
+	"strings"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello world")
+type handler struct{}
+
+func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	defer r.Body.Close()
+	code := http.StatusBadRequest
+
+	time.Sleep(time.Second * time.Duration(rand.Int31n(5)))
+
+	defer func() {
+		httpDuration := time.Since(start)
+		hist.WithLabelValues(fmt.Sprintf("%d", code)).Observe(httpDuration.Seconds())
+		ctr.WithLabelValues(fmt.Sprintf("%d", code)).Inc()
+	}()
+
+	rnd := rand.Int31n(11)
+	if rnd < 4 {
+		w.WriteHeader(code)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/plain")
+	if r.Method == "GET" {
+		code = http.StatusOK
+		vars := r.URL.Query()
+		name := vars["name"]
+		greet := fmt.Sprintf("Hello %s \n", strings.Join(name, ","))
+		w.WriteHeader(code)
+		w.Write([]byte(greet))
+	} else {
+		w.WriteHeader(code)
+	}
 }
 
+type promStruct struct{}
+
+func (h *promStruct) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+}
+
+var ctr = prometheus.NewCounterVec(prometheus.CounterOpts{
+	Name: "greetings_count",
+	Help: "Nr time greeting handler was called",
+}, []string{"code"})
+
+var hist = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+	Name:    "greeting_seconds",
+	Help:    "Time Taken to greet",
+	Buckets: []float64{1, 2, 3, 4, 5},
+}, []string{"code"})
+
+func init() {
+	prometheus.Register(hist)
+	prometheus.Register(ctr)
+	rand.Seed(time.Now().UnixNano())
+}
+
+var defHandler = new(handler)
+
 func main() {
-	http.HandleFunc("/", index)
+	http.Handle("/", defHandler)
+	http.Handle("/metrics", prometheus.Handler())
 	http.ListenAndServe(":8080", nil)
 }
