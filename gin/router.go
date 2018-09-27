09197a3621bc8cf20e74fb84fa621f6d92379222
Muhammad Falak R Wani <falakreyaz@gmail.com>
2018-09-28 01:42:17 +0530

    gin: initial commit

---
+++ gin/router.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var router *gin.Engine
+
+func main() {
+	router = gin.Default()
+
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/", func(c *gin.Context) {
+
+		c.HTML(
+			http.StatusOK,
+			"index.html",
+			gin.H{
+				"title": "Home Page",
+			},
+		)
+	})
+
+	err := router.Run()
+	if err != nil {
+		return
+	}
+}
