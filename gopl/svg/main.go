197ef1d57a0e1aadb8c720fe87d89b456e7000aa
Muhammad Falak R Wani <falakreyaz@gmail.com>
2018-10-06 08:56:30 +0530

    gopl: svg: add pprof

--- gopl/svg/main.go
+++ gopl/svg/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 	"math"
+	"os"
+	"runtime/pprof"
 )
 
 const (
-	width, height = 600, 320
-	cells         = 100
+	scale         = 100
+	width, height = scale * 600, scale * 320
+	cells         = 1000
 	xyrange       = 30.0
 	xyscale       = width / 2 / xyrange
 	zscale        = height * 0.4
@@ -16,8 +19,19 @@ const (
 
 var sin30, cos30 = math.Sin(angle), math.Cos(angle)
 
+func openPprofFile() *os.File {
+	f, err := os.Create("pprof.out")
+	if err != nil {
+		return nil
+	}
+	return f
+}
+
 func main() {
-	fmt.Printf("<svg xmlns='http://wwww.w3.org/2000/svg' "+
+	f := openPprofFile()
+	pprof.StartCPUProfile(f)
+	defer pprof.StopCPUProfile()
+	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
 		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
 		"width='%d' height='%d'>", width, height)
 	for i := 0; i < cells; i++ {
@@ -26,24 +40,24 @@ func main() {
 			bx, by := corner(i, j)
 			cx, cy := corner(i, j+1)
 			dx, dy := corner(i+1, j+1)
-			fmt.Printf("<polygon points='%g,%g %g,%g, %g,%g %g,%g' />\n",
+			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
 				ax, ay, bx, by, cx, cy, dx, dy)
 		}
 	}
 	fmt.Println("</svg>")
 }
-
 func corner(i, j int) (float64, float64) {
+	// Find point (x,y) at corner of cell (i,j).
 	x := xyrange * (float64(i)/cells - 0.5)
 	y := xyrange * (float64(j)/cells - 0.5)
-
+	// Compute surface height z.
 	z := f(x, y)
+	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
 	sx := width/2 + (x-y)*cos30*xyscale
 	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
 	return sx, sy
 }
-
 func f(x, y float64) float64 {
-	r := math.Hypot(x, y)
-	return math.Sin(r)
+	r := math.Hypot(x, y) // distance from (0,0)
+	return math.Sin(r) / r
 }
